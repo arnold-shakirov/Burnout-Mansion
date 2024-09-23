524e9d21dad47284716a1ebeb261f0d664d99db0
Arnold Shakirov <arnold.shakirov@gmail.com>
2024-09-23 16:09:37 -0400

    changed inventory with dictionary

--- Inventory.cs
+++ Inventory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public enum ItemType {note, card}
-
+/*
 public class Item
 {
     public ItemType itemtype;
@@ -15,6 +15,22 @@ public class Item
         
     }
 }
+*/
+public class Item
+{
+    public ItemType itemtype;
+    public string info;
+    public Sprite icon;
+
+    public void SetInfo(string info, ItemType type)
+    {
+        this.info = info;
+        this.itemtype = type;
+    }
+}
+//GET ITEMS IN SLOTS AND COMBINE THAT TO A DICTIONARY 
+//OF SLOTS AND INVENTORY ITEMS, KEY - SLOTS AND ITEMS ARE A VALUE.
+/*
 public class Inventory : MonoBehaviour
 {
     int maxItems;
@@ -35,6 +51,43 @@ public class Inventory : MonoBehaviour
 
         }
 
+    }
+}
+*/
+public class Inventory : MonoBehaviour
+{
+    int maxItems;
+    public List<Item> inventoryItems = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+    public Dict<GameObject, Item> slotItemDict = new Dictionary<GameObject, Item>();
+
+    void Start()
+    {
+        maxItems = slots.Count;
+    }
+
+    public void AddItem(Item item)
+    {
+        if (inventoryItems.Count < maxItems)
+        {
+            // Add item to inventoryItems
+            inventoryItems.Add(item);
+
+            // Corresponding slot and set the icon
+            GameObject slot = slots[inventoryItems.Count - 1];
+            slot.transform.GetChild(0).GetComponent<Image>().sprite = item.icon;
+
+            // Add the slot and item to the dictionary
+            slotItemDict[slot] = item;
+        }
+    }
+    public Item GetItemBySlot(GameObject slot)
+    {
+        if (slotItemDict.ContainsKey(slot))
+        {
+            return slotItemDict[slot];
+        }
+        return null;
     }
 }
 
